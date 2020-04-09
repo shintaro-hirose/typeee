cbeeee890ab91f98d1be7845246113fbb5cbcf74
shintaro-hirose <joeleshin@eis.hokudai.ac.jp>
2020-04-09 21:22:19 +0900

    Updates

---
+++ precache-manifest.502b049edeeebfcfd412d708ddac76cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "154db53a027b98f03b57445924927028",
+    "url": "/typeee/index.html"
+  },
+  {
+    "revision": "91ed51f1af8054f74d11",
+    "url": "/typeee/static/css/main.18e88231.chunk.css"
+  },
+  {
+    "revision": "78871e541f483ffa9d2c",
+    "url": "/typeee/static/js/2.b3bd6c77.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/typeee/static/js/2.b3bd6c77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91ed51f1af8054f74d11",
+    "url": "/typeee/static/js/main.ed896b02.chunk.js"
+  },
+  {
+    "revision": "1202c79885ab6502c3a7",
+    "url": "/typeee/static/js/runtime-main.d2f2801e.js"
+  },
+  {
+    "revision": "f6d8104a9ce42f318430b92e238beb90",
+    "url": "/typeee/static/media/typeee-logo.f6d8104a.svg"
+  }
+]);

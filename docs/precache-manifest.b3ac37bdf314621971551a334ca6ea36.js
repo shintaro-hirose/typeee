4e2aaec60f6f6c58c69f86db0acc8119bf6f58ef
shintaro-hirose <joeleshin@eis.hokudai.ac.jp>
2020-04-09 21:10:49 +0900

    add docs folder

---
+++ docs/precache-manifest.b3ac37bdf314621971551a334ca6ea36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a9cde0a5fa5810433b1f7aa975be69d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b7dcb70520470909962",
+    "url": "/static/css/main.18e88231.chunk.css"
+  },
+  {
+    "revision": "48fc8a5f6ec8ed3cbaf1",
+    "url": "/static/js/2.e4bcc092.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.e4bcc092.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b7dcb70520470909962",
+    "url": "/static/js/main.61f6e187.chunk.js"
+  },
+  {
+    "revision": "6422fa065f48a774ada3",
+    "url": "/static/js/runtime-main.262cb38d.js"
+  },
+  {
+    "revision": "f6d8104a9ce42f318430b92e238beb90",
+    "url": "/static/media/typeee-logo.f6d8104a.svg"
+  }
+]);
